3723c9b739cc947709c25bc04f66679ff8a65eb4
Ahmed-Omar-Hommir <ahmedomarhommir@gmail.com>
2025-08-27 13:28:33 +0200

    fix

--- lib/host.dart
+++ lib/host.dart
@@ -4,7 +4,7 @@ import 'package:analyzer_plugin/starter.dart';
 import 'package:host_plugin/src/plugin.dart';
 
 void start(List<String> args, SendPort sendPort) async {
-  ServerPluginStarter(
-    MyPlugin(),
-  ).start(sendPort);
+  // ServerPluginStarter(
+  //   MyPlugin(),
+  // ).start(sendPort);
 }
